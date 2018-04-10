b9167bfeb38ecd21f2b5b8ea1acf37645ab7be1d
朱思嘉 <zhusijia@hotoa.com>
2018-04-10 09:08:08 +0800

    无用代码

--- packages/steedos-odata/package.js
+++ packages/steedos-odata/package.js
@@ -6,11 +6,11 @@ Package.describe({
 });
 
 Npm.depends({
-	'odata-v4-parser': "git+https://github.com/steedos/odata-v4-parser#master",
-	'odata-v4-mongodb': "git+https://github.com/steedos/odata-v4-mongodb#master",
 	'basic-auth': '2.0.0',
 	'odata-v4-service-metadata': "0.1.6",
-	"odata-v4-service-document": "0.0.3"
+	"odata-v4-service-document": "0.0.3",
+	'odata-v4-parser': "git+https://github.com/steedos/odata-v4-parser#master",
+	'odata-v4-mongodb': "git+https://github.com/steedos/odata-v4-mongodb#master"
 });
 
 
