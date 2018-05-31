ce20bf6cc1c2f1d3a21db8abec32c8391fb7570f
sunhaolin <sunhaolin@hotoa.com>
2018-05-31 13:15:53 +0800

    build_vip.sh

--- build_vip.sh
+++ build_vip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-BUNDLE_PATH="/srv/creator"
+BUNDLE_PATH="/srv/vip"
 
 # Create BUNDLE_PATH directory if it does not exist
 [ ! -d $BUNDLE_PATH ] && mkdir -p $BUNDLE_PATH || :
 
 if [ -d "$BUNDLE_PATH" ]; then
-	meteor build --server https://cn.steedos.com/creator --directory $BUNDLE_PATH --allow-superuser
+	meteor build --server https://vip.steedos.com --directory $BUNDLE_PATH --allow-superuser
 	cd $BUNDLE_PATH/bundle/programs/server
 	rm -rf node_modules
 	rm -f npm-shrinkwrap.json
