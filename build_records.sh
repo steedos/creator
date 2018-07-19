0a99731dfa7c3497ce85a1e55def511a24cf30cf
zonglu <415390588@qq.com>
2018-07-19 11:05:53 +0800

    打包文件

---
+++ build_records.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BUNDLE_PATH="/srv/app-dzgy-archive"
+
+# Create BUNDLE_PATH directory if it does not exist
+[ ! -d $BUNDLE_PATH ] && mkdir -p $BUNDLE_PATH || :
+
+if [ -d "$BUNDLE_PATH" ]; then
+	meteor build --server http://192.168.0.21/dzgy_records --directory $BUNDLE_PATH --allow-superuser
+	cd $BUNDLE_PATH/bundle/programs/server
+	rm -rf node_modules
+	rm -f npm-shrinkwrap.json
+	npm install --registry https://registry.npm.taobao.org -d
+
+else
+	echo "!!!=> Failed to create bundle path: $BUNDLE_PATH"
+fi
