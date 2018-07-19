015f677c9ad51db50ee8b62633d17ab1b4630782
zonglu <415390588@qq.com>
2018-07-19 15:32:00 +0800

    打包文件

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ BUNDLE_PATH="/srv/app-dzgy-archive"
 [ ! -d $BUNDLE_PATH ] && mkdir -p $BUNDLE_PATH || :
 
 if [ -d "$BUNDLE_PATH" ]; then
-	meteor build --server https://beta.steedos.com/dzgy_records --directory $BUNDLE_PATH --allow-superuser
+	meteor build --server https://beta.steedos.com/dzgy_archive --directory $BUNDLE_PATH --allow-superuser
 	cd $BUNDLE_PATH/bundle/programs/server
 	rm -rf node_modules
 	rm -f npm-shrinkwrap.json
--- build_records.sh
+++ build_records.sh
@@ -5,7 +5,7 @@ BUNDLE_PATH="/srv/app-dzgy-archive"
 [ ! -d $BUNDLE_PATH ] && mkdir -p $BUNDLE_PATH || :
 
 if [ -d "$BUNDLE_PATH" ]; then
-	meteor build --server http://192.168.0.21/dzgy_records --directory $BUNDLE_PATH --allow-superuser
+	meteor build --server http://192.168.0.21/dzgy_archive --directory $BUNDLE_PATH --allow-superuser
 	cd $BUNDLE_PATH/bundle/programs/server
 	rm -rf node_modules
 	rm -f npm-shrinkwrap.json
