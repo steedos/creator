e1ee741e643e800e35a3283fd7217aa2e50584e8
chenzhipei <chenzhipei@hotoa.com>
2018-12-10 16:34:06 +0800

    修改bundle_path

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-BUNDLE_PATH="/srv/app-qhd-archive"
+BUNDLE_PATH="/srv/app-qhd-contract"
 
 # Create BUNDLE_PATH directory if it does not exist
 [ ! -d $BUNDLE_PATH ] && mkdir -p $BUNDLE_PATH || :
 
 if [ -d "$BUNDLE_PATH" ]; then
-	meteor build --server https://qhd.steedos.com/creator_records --directory $BUNDLE_PATH --allow-superuser
+	meteor build --server https://qhd.steedos.com/creator_contracts --directory $BUNDLE_PATH --allow-superuser
 	cd $BUNDLE_PATH/bundle/programs/server
 	rm -rf node_modules
 	rm -f npm-shrinkwrap.json
