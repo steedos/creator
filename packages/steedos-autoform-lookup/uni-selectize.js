61c8e14674315c6bebb63c24a47a602e88658e76
sunhaolin <sunhaolin@hotoa.com>
2019-01-16 16:44:28 +0800

    在对象的新建/编辑/filter/查找时，如果viewAllRecords为true，则也不限制 company_id的可选项为 自己的所属单位

--- packages/steedos-autoform-lookup/uni-selectize.js
+++ packages/steedos-autoform-lookup/uni-selectize.js
@@ -550,7 +550,11 @@ UniSelectize.prototype.getOptionsFromMethod = function (values) {
 	}
 
 	// self.loading.set(true);
-	if (this.template.data.filter_by_company && !Steedos.isSpaceAdmin()){
+	var permissions = {};
+	if (params && params.space && params.reference_to && Meteor.userId()) {
+		permissions = Creator.getPermissions(params.reference_to, params.space, Meteor.userId());
+	}
+	if (this.template.data.filter_by_company && !permissions['viewAllRecords']){
 		var companyId = Creator.getUserCompanyId();
 		if (companyId) {
 			searchVal.filterQuery.company_id = companyId;
