b71fbbbf1a8ffc2a97f66dce6c2271b439fcfa1f
Jack Zhuang <zhuangjianguo@steedos.com>
2019-01-28 19:37:41 +0800

    翻译文件发包更新到apps

--- packages/steedos-i18n/package.js
+++ packages/steedos-i18n/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'steedos:i18n',
-  version: '0.0.11',
+  version: '0.0.12',
   summary: 'i18n for Steedos',
   git: 'https://github.com/steedos/framework/packages/steedos-i18n'
 });
@@ -15,9 +15,9 @@ Package.onUse(function(api) {
   api.use('tracker');
   api.use('templating', 'client');
 
-  api.use('universe:i18n');
-  tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
-  api.addFiles(tapi18nFiles);
+  api.use('universe:i18n@1.13.0');
+  tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json'];
+  api.addFiles(tapi18nFiles, ['client', 'server']);
 
 });
 
