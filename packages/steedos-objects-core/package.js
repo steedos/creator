1017ae889ebdd9613646e0b2d5ac54e2dd04cba4
Jack Zhuang <zhuangjianguo@steedos.com>
2019-01-28 16:41:19 +0800

    发包到apps

--- packages/steedos-objects-core/package.js
+++ packages/steedos-objects-core/package.js
@@ -7,6 +7,9 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+	api.versionsFrom('METEOR@1.3');
+	
+	api.use('ecmascript');
 	api.use('reactive-var@1.0.10');
 	api.use('reactive-dict@1.1.8');
 	api.use('coffeescript@1.11.1_4');
@@ -18,16 +21,14 @@ Package.onUse(function(api) {
 	api.use('tracker@1.1.0');
 	api.use('session@1.1.6');
 	api.use('templating@1.2.15');
-	api.use('ecmascript');
 
 	api.use('aldeed:simple-schema@1.3.3');
 	api.use('steedos:cfs-standard-packages@0.5.10');
 	api.use('steedos:cfs-s3@0.1.4');
 	api.use('steedos:cfs-aliyun@0.1.0');
-	api.use('tap:i18n');
-	api.use('universe:i18n');
+	api.use('universe:i18n@1.13.0');
 	
-	api.use('steedos:objects@0.0.7');
+	api.use('steedos:objects@0.0.10');
 
 	api.addFiles('i18n.coffee');
 
