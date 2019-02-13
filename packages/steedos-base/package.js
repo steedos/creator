a784540791062f0deb00c6a57f86df8d98d143a6
yinlainghui <yinlianghui@hotoa.com>
2019-02-13 10:44:15 +0800

    #1034 升级发包steedos:base

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:base',
-	version: '0.1.7',
+	version: '0.1.8',
 	summary: 'Steedos libraries',
 	git: 'https://github.com/steedos/creator/tree/master/packages/steedos-base'
 });
