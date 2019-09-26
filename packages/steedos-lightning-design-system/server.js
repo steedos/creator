a05f281c586c1a716665ebb76bc111bd829a1e7c
庄建国 <zhuangjianguo@hotoa.com>
2019-09-26 18:22:26 +0800

    Update server.js

--- packages/steedos-lightning-design-system/server.js
+++ packages/steedos-lightning-design-system/server.js
@@ -13,7 +13,10 @@ if (projectPath.indexOf(".meteor")) {
     dsPath = path.join(projectPath, "node_modules", dsPath)
 }
 
-console.log(dsPath)
 router.use("/assets/", express.static(dsPath));
+
+if (__meteor_runtime_config__ && __meteor_runtime_config__.ROOT_URL_PATH_PREFIX)
+router.use(__meteor_runtime_config__.ROOT_URL_PATH_PREFIX + "/assets/", express.static(dsPath));
+
 
 WebApp.rawConnectHandlers.use(router);
