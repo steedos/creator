4655c993c3a056c0aee8a406ff0e56c5a50af524
hotlong <zhuangjianguo@hotoa.com>
2019-10-11 14:07:41 +0800

    手机版开关

--- client/main.js
+++ client/main.js
@@ -7,3 +7,10 @@ Template.preloadAssets.helpers({
         return Steedos.absoluteUrl(url)
     }
 });
+
+Meteor.startup(function(){
+    if (Steedos.isMobile() && Meteor.settings.public && Meteor.settings.public.mobile && Meteor.settings.public.mobile.enabled == false) {
+        $('head meta[name=viewport]').remove();
+        $('head').append('<meta name="viewport" content="">');
+    }
+});
