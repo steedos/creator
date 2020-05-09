cfca2fd2235b91035aed98a1ac3d146db8069614
殷亮辉 <yinlianghui@hotoa.com>
2020-05-09 11:42:22 +0800

    #1766  fix 在待审批暂存申请单时html字段内容丢失了

--- packages/steedos-autoform/client/coreform/inputTypes/coreform-html/html.js
+++ packages/steedos-autoform/client/coreform/inputTypes/coreform-html/html.js
@@ -1,3 +1,12 @@
 AutoForm.addInputType("steedosHtml", {
   template: "afSteedosHtml"
 });
+
+Template.afSteedosHtml.helpers({
+  attsPlusClass: function() {
+    var atts = _.clone(this.atts);
+    // Add bootstrap class
+    atts = AutoForm.Utility.addClass(atts, "form-control steedos-html");
+    return atts;
+  }
+})
