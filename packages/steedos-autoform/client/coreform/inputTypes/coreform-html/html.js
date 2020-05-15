8a38bdd8019d031235f15cd643a921ef9c32caea
殷亮辉 <yinlianghui@hotoa.com>
2020-05-15 16:48:23 +0800

    #1777 fix 待审批中的html字段只读时dom结构应该跟进行中一样，否则样式不统一

--- packages/steedos-autoform/client/coreform/inputTypes/coreform-html/html.js
+++ packages/steedos-autoform/client/coreform/inputTypes/coreform-html/html.js
@@ -1,12 +1,3 @@
 AutoForm.addInputType("steedosHtml", {
   template: "afSteedosHtml"
 });
-
-Template.afSteedosHtml.helpers({
-  attsPlusClass: function() {
-    var atts = _.clone(this.atts);
-    // Add bootstrap class
-    atts = AutoForm.Utility.addClass(atts, "form-control steedos-html");
-    return atts;
-  }
-})
